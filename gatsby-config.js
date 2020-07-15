ad384c450568cc702a43f584b289e83f695fc0a4
IGOR DOS SANTOS FERREIRA <igorferreira.noorden@sulamerica.com.br>
2020-07-15 11:20:43 -0300

    alterado conta do twitter

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
     social: {
-      twitter: `igorferreirajava`,
+      twitter: `igferreirajava`,
     },
   },
   plugins: [
